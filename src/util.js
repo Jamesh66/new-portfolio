a60ec41e17e6f3976617246328c36b5459328fe1
James Harrold <james.harrold95@gmail.com>
2021-07-01 09:18:13 +0100

    Updated url for music player image

--- src/util.js
+++ src/util.js
@@ -4,10 +4,8 @@ function cardInfo() {
   return [
     {
       title: "Bad Mouth Baking",
-      img:
-        "https://github.com/Jamesh66/new-portfolio/blob/main/src/img/badmouth.png?raw=true",
-      text:
-        "An e-commerce site I put together for a small baking company based in London. The site was constructed using the Bravada Wordpress theme along with the WooCommerce plugin to display products and handle payments.",
+      img: "https://github.com/Jamesh66/new-portfolio/blob/main/src/img/badmouth.png?raw=true",
+      text: "An e-commerce site I put together for a small baking company based in London. The site was constructed using the Bravada Wordpress theme along with the WooCommerce plugin to display products and handle payments.",
       LiveLink: "https://badmouthbaking.com/",
       GitLink: "https://github.com/Jamesh66/bad-mouth-baking",
       LinkInfo: "Live Site",
@@ -17,10 +15,8 @@ function cardInfo() {
     },
     {
       title: "Pomodoro Timer",
-      img:
-        "https://github.com/Jamesh66/new-portfolio/blob/main/src/img/pomodoro.png?raw=true",
-      text:
-        "A Pomodoro Timer I created using Vanilla JavaScript and The Materialize Front End Framework. I've since hosted the app with Netlify and currently use it on a daily basis to maintain high productivity.",
+      img: "https://github.com/Jamesh66/new-portfolio/blob/main/src/img/pomodoro.png?raw=true",
+      text: "A Pomodoro Timer I created using Vanilla JavaScript and The Materialize Front End Framework. I've since hosted the app with Netlify and currently use it on a daily basis to maintain high productivity.",
       LiveLink: "https://pomodorobyjames.com/",
       GitLink: "https://github.com/Jamesh66/pomodoro.timer",
       LinkInfo: "Visit Site",
@@ -30,10 +26,8 @@ function cardInfo() {
     },
     {
       title: "Mr Carter Maths",
-      img:
-        "https://github.com/Jamesh66/new-portfolio/blob/main/src/img/maths.png?raw=true",
-      text:
-        "An updated landing page I created for a family member using The Materialize Front End Framework. The landing page isn’t yet live due to technical difficulties in deployment unrelated to my design work.",
+      img: "https://github.com/Jamesh66/new-portfolio/blob/main/src/img/maths.png?raw=true",
+      text: "An updated landing page I created for a family member using The Materialize Front End Framework. The landing page isn’t yet live due to technical difficulties in deployment unrelated to my design work.",
       LiveLink: "https://youtu.be/iVETFjW9gqc",
       GitLink: "https://github.com/Jamesh66/mr-carter-maths/tree/main",
       LinkInfo: "Demo Video",
@@ -43,10 +37,8 @@ function cardInfo() {
     },
     {
       title: "Dev Ed Music Player",
-      img:
-        "https://github.com/Jamesh66/new-portfolio/blob/main/src/img/music.png",
-      text:
-        "This music player was a project included in the Developed By Ed React course. The player takes content from chillhop.com and plays it through the UI using React Hooks. The UI is styled using SASS.",
+      img: "https://github.com/Jamesh66/new-portfolio/blob/main/src/img/music.png?raw=true",
+      text: "This music player was a project included in the Developed By Ed React course. The player takes content from chillhop.com and plays it through the UI using React Hooks. The UI is styled using SASS.",
       LiveLink: "https://youtu.be/PPjlXRqOr2k",
       GitLink: "https://github.com/Jamesh66/dev-ed-music-player",
       LinkInfo: "Demo Video",
